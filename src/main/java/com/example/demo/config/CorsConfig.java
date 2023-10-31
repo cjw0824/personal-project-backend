ff09fb17845ce60eb84ac22e291a87dc98a905c8
cjw0824 <128335544+cjw0824@users.noreply.github.com>
2023-11-01 00:27:58 +0900

    Update CorsConfig.java

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://3.37.163.220:7777",
-                        "http://3.37.163.220:8000",
+                        "http://3.37.163.220:8080",
                         "http://127.0.0.1:8080",
                         "http://localhost:8080",
                         "http://192.168.20.3:8080",
